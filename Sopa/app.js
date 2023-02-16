d8e038239b683d8c62ea7ff0d05ed79fda6a9ee7
Fernando Canul Caballero <121654243+elegidocodes@users.noreply.github.com>
2023-02-15 19:25:10 -0600

    Little update

---
+++ Sopa/app.js
@@ -0,0 +1,241 @@
+"use strict";
+
+const img = document.getElementById("img");
+const containerInfo = document.getElementById("container-info");
+const containerLetters = document.getElementById("container-letters");
+
+const title = document.getElementById("title");
+const paragraph = document.getElementById("paragraph");
+
+img.addEventListener("click", function () {
+  containerInfo.style.opacity = "0";
+
+  setTimeout(function () {
+    containerInfo.style.zIndex = "-1";
+    containerLetters.style.opacity = "1";
+  }, 1000);
+});
+
+const letterA = document.getElementById("letter-A");
+letterA.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterC = document.getElementById("letter-C");
+letterC.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterD = document.getElementById("letter-D");
+letterD.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterE = document.getElementById("letter-E");
+letterE.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterI = document.getElementById("letter-I");
+letterI.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterL = document.getElementById("letter-L");
+letterL.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterM = document.getElementById("letter-M");
+letterM.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterN = document.getElementById("letter-N");
+letterN.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterJ = document.getElementById("letter-J");
+letterJ.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterO = document.getElementById("letter-O");
+letterO.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterP = document.getElementById("letter-P");
+letterP.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterV = document.getElementById("letter-V");
+letterV.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterS = document.getElementById("letter-S");
+letterS.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
+
+const letterT = document.getElementById("letter-T");
+letterT.addEventListener("click", function () {
+  containerLetters.style.opacity = "0";
+
+  setTimeout(function () {
+    title.innerHTML = "Abdomen";
+    paragraph.innerHTML =
+      "El abdomen es la parte del cuerpo comprendida entre el tórax y la pelvis.";
+    containerInfo.style.zIndex = "999";
+  }, 500);
+
+  setTimeout(function () {
+    containerInfo.style.opacity = "1";
+  }, 800);
+});
